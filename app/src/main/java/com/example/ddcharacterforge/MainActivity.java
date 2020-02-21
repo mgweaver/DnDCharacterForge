4e12214b8577d73ae97cdafcc39814e35d14f6d6
Mitchell Salyards <sal17030@byui.edu>
2020-02-21 11:47:08 -0700

    Added Creation Activity and button to get there.

--- app/src/main/java/com/example/ddcharacterforge/MainActivity.java
+++ app/src/main/java/com/example/ddcharacterforge/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ddcharacterforge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         //Hi Mason.
         // HIIIIIi
     }
+
+    public void creationButton(View view){
+        Intent intent = new Intent(this, CreationActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra("message", message);
+        startActivity(intent);
+    }
+
 
 }
