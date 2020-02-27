dd51a058e14c142e4999d95bb3e3dc1b6cf66a89
Mitchell Salyards <sal17030@byui.edu>
2020-02-27 12:03:36 -0700

    Added the character class.

---
+++ app/src/main/java/com/example/ddcharacterforge/Character.java
@@ -0,0 +1,18 @@
+package com.example.ddcharacterforge;
+
+
+public class Character {
+    private String name;
+    private String race;
+    private String playerClass;
+    private String background;
+    private String invortory;
+    private int hp;
+    private int strength;
+    private int constitution;
+    private int dexterity;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+
+}
