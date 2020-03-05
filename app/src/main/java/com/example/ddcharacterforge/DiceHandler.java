dfd2ed2cda34dfae4d44373ee903dbd6d8d2e74b
TabithaDefee <def17004@byui.edu>
2020-03-05 12:28:02 -0700

    Added things to Dice Handler

--- app/src/main/java/com/example/ddcharacterforge/DiceHandler.java
+++ app/src/main/java/com/example/ddcharacterforge/DiceHandler.java
@@ -2,15 +2,15 @@ package com.example.ddcharacterforge;
 
 public class DiceHandler {
 //Rolling d20
-//Math.random() * ( 20 - 1 )
+//int d20 = Math.random() * ( 20 - 1 )
 //Rolling d12
-//Math.random() * ( 12 - 1 )
+//int d12 = Math.random() * ( 12 - 1 )
 //Rolling d10
-//Math.random() * ( 10 - 1 )
+//int d10 = Math.random() * ( 10 - 1 )
 //Rolling d8
-//Math.random() * ( 8 - 1 )
+//int d8 = Math.random() * ( 8 - 1 )
 //Rolling d6
-//Math.random() * ( 6 - 1 )
+//int d6 = Math.random() * ( 6 - 1 )
 //Rolling d4
-//Math.random() * ( 4 - 1 )
+//int d4 = Math.random() * ( 4 - 1 )
 }
