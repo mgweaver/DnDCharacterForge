c4ca9175a365be23e3a35a883984a53b802e6083
TabithaDefee <def17004@byui.edu>
2020-03-05 11:48:06 -0700

    Added things to Dice Handler and adjusted the screens.

--- app/src/main/java/com/example/ddcharacterforge/DiceHandler.java
+++ app/src/main/java/com/example/ddcharacterforge/DiceHandler.java
@@ -2,4 +2,10 @@ package com.example.ddcharacterforge;
 
 public class DiceHandler {
 //onclick
+//Math.random() * ( 20 - 1 )
+//Math.random() * ( 12 - 1 )
+//Math.random() * ( 10 - 1 )
+//Math.random() * ( 8 - 1 )
+//Math.random() * ( 6 - 1 )
+//Math.random() * ( 4 - 1 )
 }
