626c0de21cf898d68a8274df1c82ff3d175d649a
TabithaDefee <def17004@byui.edu>
2020-03-06 12:13:06 -0700

    Added things to Dice Handler

--- app/src/main/java/com/example/ddcharacterforge/DiceHandler.java
+++ app/src/main/java/com/example/ddcharacterforge/DiceHandler.java
@@ -1,23 +1,24 @@
 package com.example.ddcharacterforge;
 
-public class DiceHandler extends DiceActivity {
+public class DiceHandler<d12, d20, d10, d8, d6, d4> extends DiceActivity {
 
 //Rolling d20
-    //int d20 = Math.random() * ( 20 - 1 )
+    int d20 = (int) (Math.random() * ( 20 - 1 ));
 //Rolling d12
-    //int d12 = Math.random() * ( 12 - 1 )
+    int d12 = (int) (Math.random() * ( 12 - 1 ));
 //Rolling d10
-    //int d10 = Math.random() * ( 10 - 1 )
+    int d10 = (int) (Math.random() * ( 10 - 1 ));
 //Rolling d8
-    //int d8 = Math.random() * ( 8 - 1 )
+    int d8 = (int) (Math.random() * ( 8 - 1 ));
 //Rolling d6
-    //int d6 = Math.random() * ( 6 - 1 )
+    int d6 = (int) (Math.random() * ( 6 - 1 ));
 //Rolling d4
-    //int d4 = Math.random() * ( 4 - 1 )
-    // System.out.println(d20);
-// System.out.println(d12);
-// System.out.println(d10);
-// System.out.println(d8);
-// System.out.println(d6);
-// System.out.println(d4);
+    int d4 = (int) (Math.random() * ( 4 - 1 ));
+
+    //System.out.println(d20);
+    //System.out.println(d12);
+    //System.out.println(d10);
+    //System.out.println(d8);
+    //System.out.println(d6);
+    //System.out.println(d4);
 }
