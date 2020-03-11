e8a2395f7c3d34c6a9f07f3b9f1c787c38fd1bcf
Mitchell Salyards <sal17030@byui.edu>
2020-03-11 12:11:45 -0600

    Added the class dropdown, a name input, and a save button for the creation page.

--- app/src/main/java/com/example/ddcharacterforge/CreationActivity.java
+++ app/src/main/java/com/example/ddcharacterforge/CreationActivity.java
@@ -13,13 +13,22 @@ public class CreationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner races = (Spinner) findViewById(R.id.Race);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.DnD_Races, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        races.setAdapter(adapter);
+
+        Spinner playerClasses = (Spinner) findViewById(R.id.Classes);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.DnD_Classes, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        playerClasses.setAdapter(adapter2);
     }
 }
