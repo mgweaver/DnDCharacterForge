8ca8b44ca9ef6cfbc45574c46e65863a165a68ee
mgweaver <masongweaver@gmail.com>
2020-03-11 14:32:37 -0600

    Select Character button will load JSON from testcharacter.json

--- app/src/main/java/com/example/ddcharacterforge/SelectionActivity.java
+++ app/src/main/java/com/example/ddcharacterforge/SelectionActivity.java
@@ -5,6 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 
 public class SelectionActivity extends AppCompatActivity {
 
@@ -12,6 +23,42 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+
+        Button selectChar = findViewById(R.id.selectionButton);
+        selectChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadJSON();
+            }
+        });;
+    }
+
+    private void loadJSON() {
+        InputStream is = getResources().openRawResource(R.raw.testcharacter);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String jsonString = writer.toString();
+        TextView result = findViewById(R.id.displayResult);
+        result.setText(jsonString);
     }
 
     public void creationButton(View view) {
