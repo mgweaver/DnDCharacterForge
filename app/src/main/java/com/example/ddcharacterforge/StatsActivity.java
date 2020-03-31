c6b082f876eed837656a4106282bc8c14fd45c13
Caleb St Gelais <cthesaintg@gmail.com>
2020-03-31 17:28:45 -0600

    tabs go to dice activity

--- app/src/main/java/com/example/ddcharacterforge/StatsActivity.java
+++ app/src/main/java/com/example/ddcharacterforge/StatsActivity.java
@@ -29,13 +29,6 @@ public class StatsActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
     public void diceButton(View view) {
         Intent intent = new Intent(this, DiceActivity.class);
